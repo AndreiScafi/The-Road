eabcc8364165e1672615ab928ef5e192e47ce9dc
Andrei Scafi <andreiscafi@hotmail.com>
2023-07-28 09:39:35 -0300

    Adding Style to Navbar with Javascript and CSS part 2.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+/* Navbar */
+
+const colors = ['#6495ed', '#7fffd4', '#ffa07a', '#f08080', '#afeeee'];
+
+const navLink = document.querySelectorAll('.nav-link');
+
+/* let i = 0;
+
+Array.from(navLink).forEach((item) => {
+    item.style.cssText = `background-color: ${colors[i++]}`
+});
+ */
+
+for (let i = 0; i < navLink.length; i++) {
+    navLink[i].style.cssText = `background-color: ${colors[i]}`;
+}
+/* End of Navbar */
+
+
 /* Card - Navigation - Button */
 
 const navButton = document.querySelectorAll('.navigation-button');
