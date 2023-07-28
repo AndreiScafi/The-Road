1a4a9e168ba221b456fb60bab7a43fc3112d71d1
Andrei Scafi <andreiscafi@hotmail.com>
2023-07-28 10:01:29 -0300

    Adding functionality and animations to Navbar with javascript and CSS.

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 /* Navbar */
 
-const colors = ['#6495ed', '#7fffd4', '#ffa07a', '#f08080', '#afeeee'];
+const container = document.querySelector('.container');
+const openNavbarIcon = document.querySelector('.open-navbar-icon');
+const closeNavbarIcon = document.querySelector('.close-navbar-icon');
 
+openNavbarIcon.addEventListener('click', () => {
+    container.classList.add('change');
+});
+
+closeNavbarIcon.addEventListener('click', () => {
+    container.classList.remove('change');
+});
+
+
+/* Navbar links colors */
+
+const colors = ['#6495ed', '#7fffd4', '#ffa07a', '#f08080', '#afeeee'];
 const navLink = document.querySelectorAll('.nav-link');
+
+for (let i = 0; i < navLink.length; i++) {
+    navLink[i].style.cssText = `background-color: ${colors[i]}`;
+}
 
 /* let i = 0;
 
@@ -11,9 +29,7 @@ Array.from(navLink).forEach((item) => {
 });
  */
 
-for (let i = 0; i < navLink.length; i++) {
-    navLink[i].style.cssText = `background-color: ${colors[i]}`;
-}
+/* End of Navbar links colors */
 /* End of Navbar */
 
 
